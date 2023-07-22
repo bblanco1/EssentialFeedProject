8cdabc1444f9fc957278d54441fcb83e790affba
Brian Blanco <brianblanco1@gmail.com>
2023-07-22 02:15:06 -0400

    Initial EssentialFeed Framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Brian Blanco on 7/22/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
