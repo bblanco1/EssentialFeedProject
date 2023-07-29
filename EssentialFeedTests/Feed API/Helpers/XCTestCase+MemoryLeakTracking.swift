18012149c35762678f3f27e7fafc1e1ebf03cd3a
Brian Blanco <brianblanco1@gmail.com>
2023-07-28 20:09:47 -0400

    fix file structure

--- EssentialFeedTests/Feed API/Helpers/XCTestCase+MemoryLeakTracking.swift
+++ EssentialFeedTests/Feed API/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -1,16 +1,13 @@
 //
-//  XCTestCase+MemoryLeakTracking.swift
-//  EssentialFeedTests
-//
-//  Created by Brian Blanco on 7/27/23.
+//  Copyright © 2019 Essential Developer. All rights reserved.
 //
 
 import XCTest
 
 extension XCTestCase {
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
+	func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+		addTeardownBlock { [weak instance] in
+			XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+		}
+	}
 }
